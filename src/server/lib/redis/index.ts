9840e6ca2c636f421c8c59bc54bf0ebf9db34010
mhpsy <mhpsy@proton.me>
2025-04-06 12:29:34 +0800

    feat: redis export and cursor add pnpm and test support

--- src/server/lib/redis/index.ts
+++ src/server/lib/redis/index.ts
@@ -1,11 +1,3 @@
 import redis from './redis'
 
-function redisSet(key: string, value: string) {
-  redis.set(key, value)
-}
-
-function redisGet(key: string) {
-  return redis.get(key)
-}
-
-export { redis, redisGet, redisSet }
+export { redis }
